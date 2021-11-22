debe8b64045e93f44104235483084f7e57b58001
phaniophrero <phaniophrero@yahoo.com>
2021-11-22 22:10:04 +0100

    added local settings import

--- backend/settings.py
+++ backend/settings.py
@@ -214,3 +214,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'phaniophrero@gmail.com'
 EMAIL_HOST_PASSWORD = 'Smarald168$'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
