4ad3cc0e9280d3b1c8c3604331213a7c39fedf11
phaniophrero <phaniophrero@yahoo.com>
2021-11-23 11:34:10 +0100

    updated settings file

--- backend/settings.py
+++ backend/settings.py
@@ -89,6 +89,7 @@ CORS_ALLOWED_ORIGINS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
