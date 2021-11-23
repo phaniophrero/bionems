07134e03c8652d116d8ba2c7d77caa2229fb306b
phaniophrero <phaniophrero@yahoo.com>
2021-11-23 13:57:10 +0100

    updated templates in settings file

--- backend/settings.py
+++ backend/settings.py
@@ -106,7 +106,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            #  os.path.join(BASE_DIR, 'frontend/build')
+             os.path.join(BASE_DIR, 'staticfiles/admin')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
